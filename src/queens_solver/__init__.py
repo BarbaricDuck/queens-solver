0f47da02e78427aae7d633b0d0ad6c4639fa3347
Benjamin Groves <benjaminjgroves@gmail.com>
2025-11-18 07:00:31 +0000

    feat: implement board and square classes

--- src/queens_solver/__init__.py
+++ src/queens_solver/__init__.py
@@ -5,5 +5,3 @@ _metadata = metadata("queens-solver")
 __version__ = _metadata["Version"]
 __pkg_name__ = _metadata["Name"]
 __description__ = _metadata["Summary"]
-__author__ = _metadata.get("Author")
-__license__ = _metadata.get("License")
--- src/queens_solver/main.py
+++ src/queens_solver/main.py
@@ -1,20 +1,16 @@
 import argparse
 import queens_solver
 
-def main():
+
+def main() -> None:
     parser = argparse.ArgumentParser(
-        prog=queens_solver.__pkg_name__,
-        description=queens_solver.__description__
+        prog=queens_solver.__pkg_name__, description=queens_solver.__description__
     )
 
     parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version=queens_solver.__version__
+        "-v", "--version", action="version", version=queens_solver.__version__
     )
 
     parser.parse_args()
 
     parser.print_help()
-
